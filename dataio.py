60c7c30f36b1cda6e71e34e3b51616b477cf0fb4
Ryan Gooch <s.ryan.gooch@gmail.com>
2016-04-01 14:19:43 -0600

    generalizing dataio a bit

--- dataio.py
+++ dataio.py
@@ -12,7 +12,11 @@ import pandas as pd
 from sklearn.cross_validation import train_test_split
 from sklearn.svm import OneClassSVM
 
-def getdata() :
+def getdata(trainpath = 'train.csv',testpath = 'test.csv') :
+	"""
+	Imports data in pandas dataframe format. Takes path to 
+	data. May need to alter this depending on system of use.
+	"""
 	# load data
 	df_train = pd.read_csv('train.csv')
 	df_test = pd.read_csv('test.csv')
@@ -41,5 +45,8 @@ def getdata() :
 	return df_train, df_test
 
 def writesub(id_test, y_pred, sub = "submission.csv") :
+	"""
+	Writes submission to Kaggle.
+	"""
 	submission = pd.DataFrame({"ID":id_test, "TARGET":y_pred})
 	submission.to_csv(sub, index=False)
